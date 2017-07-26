5566196f7afab0422ea88a8dacb42815d4b811e2
Aleksandr Slobodeniuk <alenuke@yandex.ru>
2017-07-26 06:26:40 +0300

    autotests with gzip and bzip2

---
+++ src/test-wrapper.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+PREFIX=$HOME/GNOME/src-new/build
+TEST_DATA_DIR=$HOME/GNOME/tst-data
+
+CC=gcc
+CFLAGS="-O0 -g -DTEST"
+
+#env vars
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
+ZFLAGS="-static $(pkg-config --cflags --libs zlib) -lbz2"
+
+#build
+echo compiling zip-dec-wrapper
+$CC -static zip-dec-wrapper.c $CFLAGS $ZFLAGS -o wrapper-test-bin
+
+#test
+mkdir $TEST_TMP_DIR
+for file in $TEST_DATA_DIR/*
+do
+    if [ -f "$file" ]; then
+	echo bzip
+	echo compressing "$file" ...
+	bzip2 -k --compress "$file"
+	if [ "$?" -eq 0 ]; then
+	    rm test_tmp_file
+	    echo decompressing "$file"...
+	    ./wrapper-test-bin "$file.bz2" test_tmp_file 1
+	    if [ "$?" -eq 0 ]; then
+		cmp "$file" test_tmp_file
+		if [ "$?" -eq 0 ]; then
+		    echo test on file "$file" completed
+		else
+		    echo wrong decoded stream
+		fi
+	    else
+                echo error during decoding
+	    fi
+	    rm "$file.bz2"
+	else
+	    echo "couldn't compress"
+	fi
+    else
+	echo "$file" is not a file
+    fi
+
+    # TODO: to func
+    if [ -f "$file" ]; then
+	echo gzip
+	echo compressing "$file" ...
+	gzip -k "$file"
+	if [ "$?" -eq 0 ]; then
+	    rm test_tmp_file
+	    echo decompressing "$file"...
+	    ./wrapper-test-bin "$file.gz" test_tmp_file 2
+	    if [ "$?" -eq 0 ]; then
+		cmp "$file" test_tmp_file
+		if [ "$?" -eq 0 ]; then
+		    echo test on file "$file" completed
+		else
+		    echo wrong decoded stream
+		fi
+	    else
+                echo error during decoding
+	    fi
+	    rm "$file.gz"
+	else
+	    echo "couldn't compress"
+	fi
+    else
+	echo "$file" is not a file
+    fi
+done
